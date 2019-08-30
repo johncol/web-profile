da0d97964efe0ed6d93524eb150ea23724714818
John <john.19col@gmail.com>
2019-08-30 12:45:05 -0500

    Updates

---
+++ precache-manifest.1c500a3a566714599262319481595d77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e049d50b136b700c350130f21728a6",
+    "url": "/web-profile/index.html"
+  },
+  {
+    "revision": "9822664712d349f9d20d",
+    "url": "/web-profile/static/css/main.642cf9e7.chunk.css"
+  },
+  {
+    "revision": "b6a1ec20116f639ddb58",
+    "url": "/web-profile/static/js/2.78cb5f0f.chunk.js"
+  },
+  {
+    "revision": "9822664712d349f9d20d",
+    "url": "/web-profile/static/js/main.fd21df1b.chunk.js"
+  },
+  {
+    "revision": "d6aa5e6f41612058805f",
+    "url": "/web-profile/static/js/runtime~main.695dd84b.js"
+  }
+]);
